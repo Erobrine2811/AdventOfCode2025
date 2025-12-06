9150fbc00d6b23c44bbaa37cff6ce3450e93ddd7
Erik Čapkovič <erikcapkovic@gmail.com>
2025-12-06 13:00:46 +0100

    d6

--- coders/erik/day_06/src/part1.rs
+++ coders/erik/day_06/src/part1.rs
@@ -13,8 +13,72 @@ where
 }
 
 fn solve(input: &[String]) -> String {
-    // Your solution for part 1 goes here
-    "solution for part 1".to_string()
+    let mut problems: Vec<Vec<i64>> = Vec::new();
+    let mut operators: Vec<String> = Vec::new();
+    let mut first = true;
+
+    let numOfLines = input.len();
+
+    let mut lineIndex = 0;
+    for line in input {
+        let rawNums = line.split(" ");
+
+        if lineIndex == numOfLines-1 { 
+            for n in rawNums {
+                if n == "" { continue; }
+
+                operators.push(n.parse::<String>().unwrap());
+            }
+
+            break;
+        }
+
+        let mut numsRow: Vec<i64> = Vec::new();
+
+
+        for n in rawNums {
+            if n == "" { continue; }
+            let asNum = n.parse::<i64>().unwrap();
+            numsRow.push(asNum);
+        }
+
+        let mut i = 0;
+        for n in numsRow {
+            if first {
+                problems.push(Vec::new());
+            }
+            problems[i].push(n);
+            i+=1;
+        }
+
+        first = false;
+        lineIndex+=1;
+    }
+
+    let mut count = 0;
+
+    let mut i = 0;
+    for problem in problems {
+        let operator = &operators[i];
+
+        let mut s = 0;
+        if operator == "*" {
+            s = 1;
+        }
+        for num in problem {
+            if operator == "*" {
+                s = s * num;
+            } else {
+                s = s + num;
+            }
+        }
+        count += s;
+
+        i+=1;
+    }
+
+    
+    return count.to_string()
 }
 
 fn main() {
--- coders/erik/day_06/src/part2.rs
+++ coders/erik/day_06/src/part2.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::File;
+use std::cmp::min;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::time::Instant;
@@ -13,8 +14,103 @@ where
 }
 
 fn solve(input: &[String]) -> String {
-    // Your solution for part 2 goes here
-    "solution for part 2".to_string()
+    let mut problems: Vec<Vec<String>> = Vec::new();
+    let mut operators: Vec<String> = Vec::new();
+    let mut first = true;
+
+    let numOfLines = input.len();
+
+    let mut lineIndex = 0;
+
+    let mut problemsRaw: Vec<String> = Vec::new();
+
+
+    for line in input {
+        let rawNums = line.split(" ");
+
+        if lineIndex == numOfLines-1 { 
+
+            let mut s = "".to_string();
+            let mut firstOperator = true;
+
+            for n in rawNums {
+                if n == "" { 
+                    s.push_str(" ");
+                    continue; 
+                }
+                if !firstOperator {
+                    problems.push(Vec::new());
+                    operators.push(s);
+                    s = "".to_string();
+                }
+                s.push_str(n);
+
+                firstOperator = false;
+            }
+            problems.push(Vec::new());
+            operators.push(s);
+
+            break;
+        }
+
+        problemsRaw.push(line.clone());
+
+        lineIndex += 1;
+    }
+
+    let mut currProblem = 0;
+
+
+    for line in problemsRaw {
+        let mut pointer = 0;
+        let lineLen = line.len();
+
+        let mut problemI = 0;
+        for op in &operators {
+            let op = &operators[problemI];
+            let op_len = op.len();
+
+            problems[problemI].push(line[pointer..min(pointer+op_len, lineLen)].to_string());
+            pointer = pointer + op_len+1;
+            problemI += 1;
+        }
+    }
+
+    let mut count: i64 = 0;
+
+    let mut problemI = 0;
+    for problem in problems {
+        let len = problem[0].len();
+        let op = operators[problemI].trim();
+
+        let mut s : i64 = 0;
+        if op == "*" {
+            s = 1;
+        }
+        
+        for index in 0..len {
+            let mut num = "".to_string();
+            for n in &problem {
+                let ch : &str = &n[index..index+1];
+                if ch == "" {continue;}
+
+                num.push_str(ch);
+            }
+            if op == "*" {
+                s = s * num.trim().parse::<i64>().unwrap();
+            }
+            else {
+                s = s + num.trim().parse::<i64>().unwrap();
+            }
+        }
+
+        count += s;
+        problemI += 1;
+    }
+
+
+    
+    return count.to_string()
 }
 
 fn main() {
